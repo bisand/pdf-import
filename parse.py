67daa2d33a2468f1a51c9a18f42e9b7d4c2f6aae
André Biseth <andre@biseth.net>
2020-10-14 20:24:54 +0200

    Testing different encoding

--- parse.py
+++ parse.py
@@ -14,24 +14,33 @@ data = slices('Dette er en test', 6, 3, 3, 10)
 
 for d in data:
     print(d)
-
-pdf = open("test.pdf", "r").read()
+encodings = ["utf-8", "ISO-8859-1", "windows-1250", "windows-1252"]
 regexPage = r"(.?)stream(.|\n)BT(?P<page>.*?)ET(.|\n)endstream"
 regexText = r"\((?P<text>.*)\)\s?Tj"
 
-matches = re.finditer(regexPage, pdf, re.DOTALL)
+for e in encodings:
+    try:
+        with open("test.pdf", "r", encoding=e) as strm:
+            pdf = strm.read()
+            
+    except UnicodeDecodeError:
+        print('got unicode error with %s , trying different encoding' % e)
+    else:
+        print('opening the file with encoding:  %s ' % e)
 
-matchesEnum = enumerate(matches, start=1)
+        matches = re.finditer(regexPage, pdf, re.DOTALL)
 
-for matchNum, match in matchesEnum:
-    pageText = match.group("page")
-    subMatches = re.finditer(regexText, pageText, re.MULTILINE)
-    subMatchesEnum = enumerate(subMatches, start=1)
+        matchesEnum = enumerate(matches, start=1)
 
-    for subMatchNum, subMatch in subMatchesEnum:
-        subGroupText = subMatch.group("text")
-        subText = "{group}".format(group=subGroupText)
-        print(subText)
+        for matchNum, match in matchesEnum:
+            pageText = match.group("page")
+            subMatches = re.finditer(regexText, pageText, re.MULTILINE)
+            subMatchesEnum = enumerate(subMatches, start=1)
 
+            for subMatchNum, subMatch in subMatchesEnum:
+                subGroupText = subMatch.group("text")
+                subText = "{group}".format(group=subGroupText)
+                print(subText)
+        break
 
 print("Finish!")
